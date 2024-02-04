43702a6be9e8879f4006eccf58a8faa6eee473b9
Tracey Thompson <150901512+larcenkoleonid81@users.noreply.github.com>
2024-02-04 19:49:28 +0530

    Create clock.swift

---
+++ clock.swift
@@ -0,0 +1,7 @@
+def main():
+    clock = pygame.time.Clock()
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
